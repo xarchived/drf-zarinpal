623aa2b86c4ec3589320b82e1ffa3ba87e08b7c3
Xurvan <xurvan@tuta.io>
2021-07-20 14:37:46 +0430

    Make urls more general

--- zarinpal/urls.py
+++ zarinpal/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from zarinpal.views import CreditPaymentRequestView, PaymentVerificationView
 
 urlpatterns = [
-    path('credit_request/', CreditPaymentRequestView.as_view()),
-    path('verification/', PaymentVerificationView.as_view())
+    path('credit_payment_request/', CreditPaymentRequestView.as_view()),
+    path('payment_verification/', PaymentVerificationView.as_view())
 ]
