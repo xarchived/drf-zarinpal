c0bfc532b9101518af3f42732db32293a95733e5
sam <alihaghparast@live.com>
2021-08-03 13:38:54 +0430

    change calculate price algo

--- zarinpal/utils.py
+++ zarinpal/utils.py
@@ -1,6 +1,20 @@
-from purchase.models import Item
+from purchase.models import Item, Order
 
 
 def calculate_total_amount(order_id: int) -> int:
     items = Item.objects.filter(order_id=order_id)
-    return sum([item.price.amount for item in items])
+    order = Order.objects.get(pk=order_id)
+    price = sum([item.price.amount for item in items])
+
+    price = price * order.duration
+
+    if order.duration == 31:
+        price = price + 5000
+
+    if order.duration == 62:
+        price = price + 7000
+
+    if order.duration == 93:
+        price = price + 9000
+
+    return price
