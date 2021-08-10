454ae326fbb8b3ba4a3358c3658198c704750e8d
Xojtaba <xurvan@tutanota.com>
2021-08-10 16:28:24 +0430

    Fix item selection

--- zarinpal/utils.py
+++ zarinpal/utils.py
@@ -4,4 +4,4 @@ from purchase.models import Order
 def calculate_total_amount(order_id: int) -> int:
     order = Order.objects.get(pk=order_id)
 
-    return sum([item.price.amount for item in order.items])
+    return sum([item.price.amount for item in order.items.all()])
