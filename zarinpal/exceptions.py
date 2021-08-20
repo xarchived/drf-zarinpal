2d0e6ee39cffb001f3e694898ed6def89540f8a4
Xurvan <xurvan@tutanota.com>
2021-08-20 15:36:24 +0430

    Replace HTTP status code with DRF statics

--- zarinpal/exceptions.py
+++ zarinpal/exceptions.py
@@ -1,12 +1,13 @@
-from rest_framework.exceptions import APIException
 from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.exceptions import APIException
 
 
 class OrderNotFoundError(APIException):
-    status_code = 404
+    status_code = status.HTTP_404_NOT_FOUND
     default_detail = _('order not found')
 
 
 class PaymentError(APIException):
-    status_code = 400
+    status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _('order not found')
